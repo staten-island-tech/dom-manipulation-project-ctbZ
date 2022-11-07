2b3aa3cc33a7430128bd7809295588f091b80d19
ctbZ <113132299+ctbZ@users.noreply.github.com>
2022-11-07 13:16:24 -0500

    L1 selecting elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const DOMSelectors = {
+    button:document.getElementById("btn"),
+    text: documument.querySelector("#text"),
+};
+console.log()
+const button = document.getElementById("btn")
+const text = document.querySelector("#text")
+
+console.log(DOMSelectors.button);
